fbe36af1bb4646f681672ff07f705e68d7c11394
ravi ranjan <2709.rravi@gmail.com>
2019-07-06 00:42:30 +0530

    first commit

---
+++ Phonebook/ProfileDetailController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileDetailController.swift
+//  Phonebook
+//
+//  Created by Ravi Singh on 05/07/19.
+//  Copyright © 2019 ravi singh. All rights reserved.
+//
+
+import UIKit
+
+class ProfileDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
